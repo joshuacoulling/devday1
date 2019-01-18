2a6483dfc933c9179fb5748a6f609873245b6fa9
Josh Coulling <jcoulling@jcoulling-m01.vmware.com>
2019-01-18 15:01:56 +0000

    first commit

---
+++ Scratch.js
@@ -0,0 +1,15 @@
+// console.log('hello world')
+const a=1
+const b=2
+const c=a+b
+// console.log("result",c)
+
+const foo = ( num1, num2 )=> {
+  const  result = num1 * num2
+  return result;
+}
+
+const multi = foo
+
+const test =  multi(2,3)
+console.log("my function",foo)
